a0feb967a26e98049a28bb6bb43fc27e14dec8b7
eirenaios <suresh.onezero@gmail.com>
2014-12-27 16:56:58 +0530

    Navigation Bar with minimal design. using only css.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="navbar.css" type="text/css" rel="stylesheet">
+        <title>Rounded Navigation Bar Using CSS</title>
+    </head>
+    <body>
+        <!--Navigation Starts-->
+        <ul>
+            <li><a href="#">Navigation</a></li>
+            <li class="active"><a href="#">Nav</a></li>
+            <li><a href="#">Nav</a></li>
+            <li><a href="active">Nav</a></li>
+            <li class="active"><a href="#">Lat Nav</a></li>
+        </ul>
+        <!--Navigation Ends-->
+        <div style="clear:both;"></div>
+        <!--Container Starts-->
+        <div class="container">
+        </div>
+        <!--Container Ends-->
+    </body>
+</html>
